0c53da089b511cd7e17a2e24ba8f7238ecb3f049
Mykola Postoiko <dotnicht@live.com>
2023-06-26 23:40:16 +0300

    fingerprint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
+use serde::Deserialize;
+use wasm_fingerprint::*;
 use yew::prelude::*;
+
+#[derive(Deserialize)]
+struct Fingerprint {
+    print: String,
+}
 
 #[function_component(App)]
 fn app() -> Html {
+    let fingerprint = make_fingerprint().unwrap();
+    let fingerprint: Fingerprint = serde_json::from_str(&fingerprint).unwrap();
+
     html! {
         <div>
-            <h1>{ "Paula Brillant!" }</h1>
+            <h1>{ fingerprint.print }</h1>
             <footer>
                 <a href="mailto:generated@ebobo.dev">{"generated@ebobo.dev"}</a>
             </footer>
