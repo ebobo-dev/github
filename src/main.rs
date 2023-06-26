006fdbda32755d82d2ed4c2560d90fb9cd1cee9e
Mykola Postoiko <dotnicht@live.com>
2023-06-27 00:14:53 +0300

    title

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,15 @@ fn app() -> Html {
     let fingerprint = make_fingerprint().unwrap();
     let fingerprint: Fingerprint = serde_json::from_str(&fingerprint).unwrap();
 
+    if fingerprint.print == "10E14AEC" {
+        let window = web_sys::window().unwrap();
+        let document = window.document().unwrap();
+        document.set_title("привет еблан!");
+    }
+
     html! {
         <div>
-            <h1>{ fingerprint.print }</h1>
+            <h1>{ "ebobo.dev" }</h1>
             <footer>
                 <a href="mailto:generated@ebobo.dev">{"generated@ebobo.dev"}</a>
             </footer>
