3d7bea12d2023661a0cc75563df7416241075141
Mykola Postoiko <dotnicht@live.com>
2023-06-27 22:34:49 +0300

    title

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use serde::Deserialize;
 use wasm_fingerprint::*;
+use web_sys::*;
 use yew::prelude::*;
 
 #[derive(Deserialize)]
@@ -9,18 +10,16 @@ struct Fingerprint {
 
 #[function_component(App)]
 fn app() -> Html {
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+    document.set_title("ebobo.dev");
+
     let fingerprint = make_fingerprint().unwrap();
     let fingerprint: Fingerprint = serde_json::from_str(&fingerprint).unwrap();
 
-    if fingerprint.print == "10E14AEC" {
-        let window = web_sys::window().unwrap();
-        let document = window.document().unwrap();
-        document.set_title("привет еблан!");
-    }
-
     html! {
         <div>
-            <h1>{ "ebobo.dev" }</h1>
+            <h1>{ fingerprint.print }</h1>
             <footer>
                 <a href="mailto:generated@ebobo.dev">{"generated@ebobo.dev"}</a>
             </footer>
