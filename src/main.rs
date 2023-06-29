1f87ea26e13fce31c85839e726c4a5cb12e45b8e
Mykola Postoiko <dotnicht@live.com>
2023-06-30 01:49:23 +0300

    component

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,35 @@ struct Fingerprint {
     print: String,
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let window = window().unwrap();
-    let document = window.document().unwrap();
-    document.set_title("ebobo.dev");
-
+#[function_component(WhoAmI)]
+fn fingerprint() -> Html {
     let fingerprint = make_fingerprint().unwrap();
     let fingerprint: Fingerprint = serde_json::from_str(&fingerprint).unwrap();
 
     html! {
         <div>
-            <h1>{ fingerprint.print }</h1>
+            <h2>{ format!("Fingerprint: {}", fingerprint.print) }</h2>
+        </div>
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let window = window().unwrap();
+    let document = window.document().unwrap();
+
+    const NAME: &str = "ebobo.dev";
+
+    document.set_title(NAME);
+
+    let email = format!("generated@{}", NAME);
+
+    html! {
+        <div>
+            <h1>{ "Hello, Ebobo!" } </h1>
+            <WhoAmI />
             <footer>
-                <a href="mailto:generated@ebobo.dev">{"generated@ebobo.dev"}</a>
+                <a href={ format!("mailto:{}", email) }>{ email }</a>
             </footer>
         </div>
     }
