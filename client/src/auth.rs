02ee613d1797ddbeb77f0340821ad806af558b94
dotnicht <dotnicht@ebobo.dev>
2024-03-09 22:19:45 +0200

    ip

--- client/src/auth.rs
+++ client/src/auth.rs
@@ -3,12 +3,19 @@ use reqwasm::http::Request;
 use serde::Deserialize;
 use sycamore::prelude::*;
 use wasm_fingerprint::make_fingerprint;
-use web_sys::window;
 
 #[component]
 pub async fn Auth<G: Html>() -> View<G> {
     let fingerprint: Fingerprint = serde_json::from_str(&make_fingerprint().unwrap()).unwrap();
-    let addr = window().unwrap().location().host().unwrap();
+    
+    let addr = Request::get("https://api.ipify.org")
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
     let greet = post(&fingerprint.print, &addr).await.unwrap();
 
     view! {
@@ -19,7 +26,8 @@ pub async fn Auth<G: Html>() -> View<G> {
 }
 
 async fn post(fingerprint: &str, host: &str) -> Result<String, reqwasm::Error> {
-    let url = std::env::var("EBOBO_AUTH_URL").unwrap_or_else(|_| "http://localhost:8000/authenticate".to_string());
+    let url = std::env::var("EBOBO_AUTH_URL")
+        .unwrap_or_else(|_| "http://localhost:8000/authenticate".to_string());
     Ok(Request::post(&url)
         .body(
             serde_json::to_string(&Auth {
@@ -29,11 +37,12 @@ async fn post(fingerprint: &str, host: &str) -> Result<String, reqwasm::Error> {
             .unwrap(),
         )
         .send()
-        .await?.text().await?)
+        .await?
+        .text()
+        .await?)
 }
 
 #[derive(Deserialize)]
 struct Fingerprint {
     print: String,
 }
-
