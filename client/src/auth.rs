ebc11d5dee3f5d7c3d100bd143efb04d6b463920
dotnicht <dotnicht@ebobo.dev>
2024-03-09 23:09:51 +0200

    shuttle default

--- client/src/auth.rs
+++ client/src/auth.rs
@@ -29,7 +29,7 @@ pub async fn Auth<G: Html>() -> View<G> {
 
 async fn post(fingerprint: &str, host: &str) -> Result<String, reqwasm::Error> {
     let url = std::env::var("EBOBO_AUTH_URL")
-        .unwrap_or_else(|_| "http://localhost:8000/authenticate".to_string());
+        .unwrap_or_else(|_| "https://ebobo.shuttleapp.rs/authenticate".to_string());
     Ok(Request::post(&url)
         .body(
             serde_json::to_string(&Auth {
