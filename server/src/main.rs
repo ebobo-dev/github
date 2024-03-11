5a030550c8345bd181d0b48033a9813964baba6a
dotnicht <dotnicht@ebobo.dev>
2024-03-11 03:33:07 +0200

    format

--- server/src/main.rs
+++ server/src/main.rs
@@ -7,16 +7,19 @@ extern crate rocket;
 
 use fairings::*;
 use handlers::*;
+use qdrant_client::prelude::*;
 use shuttle_persist::PersistInstance;
 use shuttle_secrets::SecretStore;
-use qdrant_client::prelude::*;
 
 #[shuttle_runtime::main]
 async fn rocket(
     #[shuttle_secrets::Secrets] _secrets: SecretStore,
     #[shuttle_persist::Persist] persist: PersistInstance,
-    #[shuttle_qdrant::Qdrant(cloud_url = "{secrets.CLOUD_URL}", api_key = "{secrets.API_KEY}")]
-    qdrant: QdrantClient
+    #[shuttle_qdrant::Qdrant(
+        cloud_url = "https://25197a8d-dcf0-497d-97e2-ce57cdbdae57.us-east4-0.gcp.cloud.qdrant.io",
+        api_key = "{secrets.DB_QDRANT_TOKEN}"
+    )]
+    _qdrant: QdrantClient,
 ) -> shuttle_rocket::ShuttleRocket {
     let state = auth::AuthState { persist };
 
