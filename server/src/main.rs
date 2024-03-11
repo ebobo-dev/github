30c2fb14bad95126988ece9006c0f36e47dabed2
dotnicht <dotnicht@ebobo.dev>
2024-03-11 14:48:43 +0200

    qdrant

--- server/src/main.rs
+++ server/src/main.rs
@@ -11,6 +11,9 @@ use qdrant_client::prelude::*;
 use shuttle_persist::PersistInstance;
 use shuttle_secrets::SecretStore;
 
+use qdrant_client::qdrant::vectors_config::Config;
+use qdrant_client::qdrant::{CreateCollection, VectorParams, VectorsConfig};
+
 #[shuttle_runtime::main]
 async fn rocket(
     #[shuttle_secrets::Secrets] _secrets: SecretStore,
@@ -19,7 +22,7 @@ async fn rocket(
         cloud_url = "https://25197a8d-dcf0-497d-97e2-ce57cdbdae57.us-east4-0.gcp.cloud.qdrant.io",
         api_key = "{secrets.DB_QDRANT_TOKEN}"
     )]
-    _qdrant: QdrantClient,
+    qdrant: QdrantClient,
 ) -> shuttle_rocket::ShuttleRocket {
     let state = auth::AuthState { persist };
 
@@ -27,6 +30,20 @@ async fn rocket(
         .attach(cors::CORS)
         .mount("/", routes![auth::authenticate, admin::reset, admin::index])
         .manage(state);
+
+    qdrant
+        .create_collection(&CreateCollection {
+            collection_name: "test".to_owned(),
+            vectors_config: Some(VectorsConfig {
+                config: Some(Config::Params(VectorParams {
+                    size: 10,
+                    distance: Distance::Cosine.into(),
+                    ..Default::default()
+                })),
+            }),
+            ..Default::default()
+        })
+        .await?;
 
     Ok(rocket.into())
 }
