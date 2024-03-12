65592c869ca378d84bb376af753f998839404e9c
dotnicht <dotnicht@ebobo.dev>
2024-03-12 21:08:09 +0200

    restore admin

--- server/src/handlers/admin.rs
+++ server/src/handlers/admin.rs
@@ -1,26 +1,42 @@
 use crate::domain::Device;
+use crate::AppState;
 use libsql::params;
 use rocket::response::status::BadRequest;
 use rocket::serde::json::Json;
 use rocket::State;
+use std::sync::Arc;
 
 #[get("/admin")]
-pub fn index(conn: &State<crate::Database>) -> Result<Json<Vec<Device>>, BadRequest<String>> {
-    let devices = conn.turso.query("SELECT * FROM Devices", params![])
-        .persist
-        .list()
-        .unwrap()
-        .iter()
-        .map(|f| state.persist.load::<Device>(f).unwrap())
-        .collect();
+pub async fn index(state: &State<Arc<AppState>>) -> Result<Json<Vec<Device>>, BadRequest<String>> {
+    let mut res = state
+        .db
+        .lock()
+        .await
+        .query("SELECT * FROM devices", params![])
+        .await
+        .unwrap();
+
+    let mut devices = vec![];
+
+    while let Some(device) = res.next().unwrap() {
+        devices.push(Device {
+            id: device.get(0).unwrap(),
+            fingerprint: device.get(1).unwrap(),
+            fighter: None,
+        });
+    }
+
     Ok(Json(devices))
 }
 
 #[post("/admin/reset")]
-pub fn reset(conn: &State<crate::Database>) -> Result<(), BadRequest<String>> {
-    conn.turso.query("DELETE FROM Devices", params![])
-        .persist
-        .reset()
+pub async fn reset(state: &State<Arc<AppState>>) -> Result<(), BadRequest<String>> {
+    state
+        .db
+        .lock()
+        .await
+        .execute("DELETE FROM devices", params![])
+        .await
         .unwrap();
     Ok(())
 }
