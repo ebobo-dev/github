508c156a8daee393a7f4d831cddf6555718060ab
dotnicht <dotnicht@ebobo.dev>
2024-03-13 00:31:06 +0200

    click

--- client/src/fight.rs
+++ client/src/fight.rs
@@ -2,28 +2,26 @@ use sycamore::prelude::*;
 
 #[component]
 pub async fn Fight<G: Html>() -> View<G> {
-
     let fighters = create_signal(vec!["🐱", "🐵", "🐶"]);
-
-    let choose = move |e| {
-    };
+    let fighter: Signal<Option<&str>> = create_signal(None);
 
     view! {
         div {
             p { "choose your fighter:" }
             ul {
                 Indexed(
-                    iterable=*fighters,
-                    view=|f| view! {
+                    iterable = *fighters,
+                    view = move |f| view! {
                         li {
-                            a(href=format!("https://www.youtube.com/watch?v={f}")) {
-                                (f)
-                            }
+                            button(on:click=move |_| fighter.set(Some(f))) { (f) }
                         }
-                    }                )
+                    }
+                )
             }
-
-            button(on:click=choose) { "Choose" }
+            p {
+                "you chose: "
+                (fighter.get().unwrap_or("nothing"))
+            }
         }
     }
 }
