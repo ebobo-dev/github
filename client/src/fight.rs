f74442405de2232cedc59d312647948c4488b1fa
dotnicht <dotnicht@ebobo.dev>
2024-03-13 02:18:14 +0200

    save

--- client/src/fight.rs
+++ client/src/fight.rs
@@ -5,6 +5,14 @@ pub async fn Fight<G: Html>() -> View<G> {
     let fighters = create_signal(vec!["🐱", "🐵", "🐶", "🐷"]);
     let fighter: Signal<Option<&str>> = create_signal(None);
 
+    let save = {
+        let fighter = fighter.clone();
+        move |_| {
+            let f = fighter.get().unwrap();
+            let _ = post(f);
+        }
+    };
+
     view! {
         div {
             p { "choose your fighter:" }
@@ -21,7 +29,25 @@ pub async fn Fight<G: Html>() -> View<G> {
             p {
                 "you chose: "
                 (fighter.get().unwrap_or("nothing"))
+                button(on:click=save) { "save" }
             }
         }
     }
 }
+
+async fn post(fighter: &str) -> Result<(), reqwasm::Error> {
+    match reqwasm::http::Request::post("https://ebobo.shuttleapp.rs/choose")
+            .body(
+                serde_json::to_string(&ebobo_shared::Fighter {
+                    fingerprint: "fingerprint".to_string(),
+                    fighter: Some(fighter.to_string()),
+                })
+                .unwrap(),
+            )
+            .send()
+            .await {
+        Ok(_) => Ok(()),
+        Err(err) => return Err(err),
+    }
+}
+
