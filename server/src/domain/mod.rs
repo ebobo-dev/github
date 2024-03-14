70751607e89428eb74bf67ccef9b5a9db89c7016
dotnicht <dotnicht@ebobo.dev>
2024-03-15 01:51:47 +0200

    get

--- server/src/domain/mod.rs
+++ server/src/domain/mod.rs
@@ -2,13 +2,11 @@ use ebobo_shared::*;
 
 #[derive(Serialize, Deserialize)]
 pub struct Device {
-    pub id: i32,
     pub fingerprint: String,
     pub fighter: Option<char>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct Location {
-    pub id: i32,
     pub address: String
 }
--- server/src/handlers/admin.rs
+++ server/src/handlers/admin.rs
@@ -1,18 +1,30 @@
 use crate::domain::Device;
+use crate::entities::prelude::*;
 use rocket::response::status::BadRequest;
 use rocket::serde::json::Json;
 use rocket::State;
-use sea_orm::DatabaseConnection;
+use sea_orm::{DatabaseConnection, EntityTrait};
 use std::sync::Arc;
 
 #[get("/admin")]
 pub async fn index(
     state: &State<Arc<DatabaseConnection>>,
 ) -> Result<Json<Vec<Device>>, BadRequest<String>> {
-    panic!()
+    let devices = Devices::find().all(state.as_ref()).await.unwrap();
+
+    let devices = devices
+        .into_iter()
+        .map(|device| Device {
+            fingerprint: device.fingerprint,
+            fighter: None,
+        })
+        .collect::<Vec<Device>>();
+
+    Ok(Json(devices))
 }
 
 #[post("/admin/reset")]
 pub async fn reset(state: &State<Arc<DatabaseConnection>>) -> Result<(), BadRequest<String>> {
+    panic!("Not implemented");
     Ok(())
 }
