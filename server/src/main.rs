401accce5acb01f71203c638801ecc58773180c3
dotnicht <dotnicht@ebobo.dev>
2024-03-15 00:43:54 +0200

    postgres

--- server/src/main.rs
+++ server/src/main.rs
@@ -8,13 +8,15 @@ extern crate rocket;
 use fairings::*;
 use handlers::*;
 use sea_orm::*;
+use shuttle_secrets::SecretStore;
 use std::sync::Arc;
 
 #[shuttle_runtime::main]
 async fn rocket(
-    #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    // #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
+    #[shuttle_secrets::Secrets] secret_store: SecretStore
 ) -> shuttle_rocket::ShuttleRocket {
-    let conn = Database::connect("postgresql://neondb_owner:L5EhgjFndt6R@ep-royal-math-a2j2dztz-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require");
+    let conn = Database::connect(secret_store.get("DB_CONNECTION_STRING").unwrap());
 
     let rocket = rocket::build()
         .attach(cors::CORS)
