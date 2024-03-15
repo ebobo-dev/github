eeb1c4a8d47c6e876c39cc2637339b72b513af77
dotnicht <dotnicht@ebobo.dev>
2024-03-15 22:07:10 +0200

    clean

--- server/src/main.rs
+++ server/src/main.rs
@@ -28,7 +28,6 @@ async fn rocket(
             routes![
                 auth::authenticate,
                 fight::choose,
-                admin::reset,
                 admin::index
             ],
         );
