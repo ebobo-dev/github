93153326d6b7893f750c00375aa819ed9b551f73
dotnicht <dotnicht@ebobo.dev>
2024-03-15 13:32:03 +0200

    fight

--- server/src/handlers/fight.rs
+++ server/src/handlers/fight.rs
@@ -1,9 +1,10 @@
-use crate::entities::prelude::*;
+use crate::entities::{devices::*, prelude::*};
 use ebobo_shared::*;
 use rocket::response::status::BadRequest;
 use rocket::serde::json::Json;
 use rocket::State;
 use sea_orm::prelude::*;
+use sea_orm::*;
 use std::sync::Arc;
 
 #[post("/choose", data = "<request>")]
@@ -11,7 +12,27 @@ pub async fn choose(
     request: Json<Fighter>,
     state: &State<Arc<DatabaseConnection>>,
 ) -> Result<(), BadRequest<String>> {
-    
+    let device = Devices::find()
+        .filter(Column::Fingerprint.eq(request.fingerprint.clone()))
+        .one(state.as_ref())
+        .await
+        .map_err(|e| BadRequest(format!("Failed to find device: {}", e.to_string())))?;
 
-    Ok(())
+    match device {
+        Some(device) => {
+            let device = ActiveModel {
+                id: ActiveValue::unchanged(device.id),
+                fingerprint: ActiveValue::unchanged(request.fingerprint.clone()),
+                fighter: ActiveValue::set(request.fighter.clone()),
+            };
+
+            Devices::update(device)
+                .exec(state.as_ref())
+                .await
+                .map_err(|e| BadRequest(format!("Failed to update device: {}", e.to_string())))?;
+
+            Ok(())
+        }
+        None => Err(BadRequest("Failed to find device".to_string())),
+    }
 }
