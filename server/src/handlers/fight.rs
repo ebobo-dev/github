262bdcd6c082d824e501bf30a464dac6068b22e3
dotnicht <dotnicht@ebobo.dev>
2024-03-15 02:31:41 +0200

    temp

--- server/src/handlers/fight.rs
+++ server/src/handlers/fight.rs
@@ -1,16 +1,16 @@
+use crate::entities::prelude::*;
 use ebobo_shared::*;
 use rocket::response::status::BadRequest;
 use rocket::serde::json::Json;
 use rocket::State;
-use std::sync::Arc;
 use sea_orm::prelude::*;
+use std::sync::Arc;
 
 #[post("/choose", data = "<request>")]
 pub async fn choose(
     request: Json<Fighter>,
     state: &State<Arc<DatabaseConnection>>,
 ) -> Result<(), BadRequest<String>> {
-
     
 
     Ok(())
