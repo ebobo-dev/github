503a11190872f33389a986a603f54e1425e853d7
dotnicht <dotnicht@ebobo.dev>
2024-03-15 04:15:03 +0200

    fix unique

--- server/src/handlers/auth.rs
+++ server/src/handlers/auth.rs
@@ -1,22 +1,30 @@
+use crate::entities::{devices::*, prelude::*};
 use ebobo_shared::*;
 use rocket::response::status::BadRequest;
 use rocket::serde::json::Json;
 use rocket::State;
-use sea_orm::{ActiveValue, DatabaseConnection, EntityTrait};
+use sea_orm::*;
 use std::sync::Arc;
-use crate::entities::{devices::*, prelude::*};
 
 #[post("/authenticate", data = "<request>")]
 pub async fn authenticate(
     request: Json<Auth>,
     state: &State<Arc<DatabaseConnection>>,
 ) -> Result<Json<Fighter>, BadRequest<String>> {
-    let device = ActiveModel {
-        fingerprint: ActiveValue::set(request.fingerprint.clone()),
-        ..Default::default()
-    };
+    let device = Devices::find()
+        .filter(Column::Fingerprint.eq(request.fingerprint.clone()))
+        .one(state.as_ref())
+        .await
+        .unwrap();
 
-    Devices::insert(device).exec(state.as_ref()).await.unwrap();
+    if device.is_none() {
+        let device = ActiveModel {
+            fingerprint: ActiveValue::set(request.fingerprint.clone()),
+            ..Default::default()
+        };
+
+        Devices::insert(device).exec(state.as_ref()).await.unwrap();
+    }
 
     Ok(Json(Fighter {
         fingerprint: request.fingerprint.clone(),
--- server/src/main.rs
+++ server/src/main.rs
@@ -14,7 +14,6 @@ use std::sync::Arc;
 
 #[shuttle_runtime::main]
 async fn rocket(
-    // #[shuttle_shared_db::Postgres] pool: sqlx::PgPool,
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_rocket::ShuttleRocket {
     let conn = Database::connect(secret_store.get("DB_CONNECTION_STRING").unwrap())
