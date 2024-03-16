a1afd6af7da4b343e6be7e5ad9aaf9fac9a53259
dotnicht <dotnicht@ebobo.dev>
2024-03-16 23:57:30 +0200

    rename

--- server/src/main.rs
+++ server/src/main.rs
@@ -14,15 +14,15 @@ use std::sync::Arc;
 
 #[shuttle_runtime::main]
 async fn rocket(
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    #[shuttle_secrets::Secrets] secrets: SecretStore,
 ) -> shuttle_rocket::ShuttleRocket {
-    let conn = Database::connect(secret_store.get("DB_CONNECTION_STRING").expect("Connection string not found"))
+    let conn = Database::connect(secrets.get("DB_CONNECTION_STRING").expect("Connection string not found"))
         .await
         .expect("Failed to connect to the database");
 
     let rocket = rocket::build()
         .manage(Arc::new(conn))
-        .manage(Arc::new(secret_store))
+        .manage(Arc::new(secrets))
         .attach(cors::CORS)
         .mount(
             "/",
