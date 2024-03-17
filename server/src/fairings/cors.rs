c9a937d506c1ae2ca5cf82bb4f1579bd032f050b
dotnicht <dotnicht@ebobo.dev>
2024-03-17 14:31:21 +0200

    format

--- server/src/fairings/cors.rs
+++ server/src/fairings/cors.rs
@@ -1,6 +1,6 @@
+use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 use rocket::{Request, Response};
-use rocket::fairing::{Fairing, Info, Kind};
 
 pub struct CORS;
 
@@ -9,14 +9,23 @@ impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
             name: "Add CORS headers to responses",
-            kind: Kind::Response
+            kind: Kind::Response,
         }
     }
 
     async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
-        response.set_header(Header::new("Access-Control-Allow-Headers", request.headers().get_one("Access-Control-Request-Headers").unwrap_or("*")));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Headers",
+            request
+                .headers()
+                .get_one("Access-Control-Request-Headers")
+                .unwrap_or("*"),
+        ));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
 }
