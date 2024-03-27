dac390147164662c580d2266db047a76dd02902b
dotnicht <dotnicht@ebobo.dev>
2024-03-27 17:13:44 +0200

    format

--- client/src/components/footer.rs
+++ client/src/components/footer.rs
@@ -4,9 +4,15 @@ use web_sys::window;
 #[component]
 pub fn Footer<G: Html>() -> View<G> {
     const NAME: &str = "ebobo.dev";
+
     let email = format!("generated@{}", NAME);
     let link = format!("mailto:{}", email);
-    window().unwrap().document().unwrap().set_title(NAME);
+
+    window()
+        .expect("Window not accessible")
+        .document()
+        .expect("Document not accessible")
+        .set_title(NAME);
 
     view! {
         footer {
