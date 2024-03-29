25caf2a108a7742f0eb29f7aac95076ac47c9d9d
dotnicht <dotnicht@ebobo.dev>
2024-03-30 01:17:33 +0200

    comment index

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -4,21 +4,11 @@ use crate::api::get;
 
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
-    let fighters = create_signal(get().await.expect("Index call failed"));
+    //let fighters = create_signal(get().await.expect("Index call failed"));
 
     view! {
         p {
             "Fighters:"
-            ul {
-                Indexed(
-                    iterable = *fighters,
-                    view = move |f| view! {
-                        li {
-//                            (format!("{}: {}", f.fingerprint, f.fighter.as_deref().unwrap_or("Anonymous")))
-                        }
-                    }
-                )
-            }
         }
     }
 }
