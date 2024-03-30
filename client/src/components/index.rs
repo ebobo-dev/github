64bdf1f42834a6fd3f1c61160c75b4242a33309f
dotnicht <dotnicht@ebobo.dev>
2024-03-30 21:40:34 +0200

    debug

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -4,12 +4,15 @@ use crate::api::get;
 
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
-    let fighters = get().await.expect("Index call failed");
-    let state = create_signal(fighters);
+    let fighters = get().await;
+    let greet = match fighters {
+        Ok(fighters) => fighters.len().to_string(),
+        Err(e) => e.to_string(),
+    };
 
     view! {
         p {
-            "Fighters:"
+            (greet)
         }
     }
 }
