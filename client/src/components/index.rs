3ce08461962733259364a977bb0f0e2ed27d06f5
dotnicht <dotnicht@ebobo.dev>
2024-03-30 14:09:43 +0200

    clean

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -1,10 +1,8 @@
 use sycamore::prelude::*;
 
-use crate::api::get;
-
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
-    let fighters = create_signal(get().await.expect("Index call failed"));
+    //let fighters = create_signal(get().await.expect("Index call failed"));
 
     view! {
         p {
