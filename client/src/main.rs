e3333ee917832d196887a85c911c98449e568189
dotnicht <dotnicht@ebobo.dev>
2024-04-09 02:36:33 +0300

    title

--- client/src/main.rs
+++ client/src/main.rs
@@ -8,6 +8,8 @@ mod components;
 
 #[component]
 fn App<G: Html>() -> View<G> {
+    window().unwrap().document().unwrap().set_title("ebobo.dev");
+
     view! {
         div {
             article {
@@ -20,7 +22,6 @@ fn App<G: Html>() -> View<G> {
 }
 
 fn main() {
-    window().unwrap().document().unwrap().set_title("ebobo.dev");
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Debug).unwrap();
     sycamore::render(App);
