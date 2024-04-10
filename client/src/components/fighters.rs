faf54ab42ff1eb9e771281f57d2cff36a2d2cc6c
dotnicht <dotnicht@ebobo.dev>
2024-04-10 22:16:05 +0300

    refactoring

---
+++ client/src/components/fighters.rs
@@ -0,0 +1,46 @@
+use sycamore::futures::spawn_local;
+use sycamore::prelude::*;
+
+use ebobo_shared::Fighter;
+
+use crate::api::*;
+
+#[component(inline_props)]
+pub async fn Fighters<G: Html>(fighters: Vec<Fighter>) -> View<G> {
+    let available = create_signal(fighters);
+
+    view!( div {
+        p { "choose your fighter:" }
+        ul {
+            Indexed(
+                iterable = *available,
+                view = move |f| view! {
+                    SelectFighter(fighter = f.clone())
+                }
+            )
+        }
+    })
+}
+
+#[component(inline_props)]
+pub async fn SelectFighter<G: Html>(fighter: Fighter) -> View<G> {
+    let name = fighter.clone().name();
+    let selected: Signal<Option<Fighter>> = create_signal(None);
+
+    create_effect(move || {
+        if let Some(fighter) = selected.get_clone() {
+            spawn_local(async move {
+                match choose(fighter.name()).await {
+                    Ok(_) => (),
+                    Err(err) => log::error!("error: {:?}", err),
+                }
+            });
+        }
+    });
+
+    view! {
+        li {
+            button(on:click = move |_| selected.set(Some(fighter.clone()))) { (name) }
+        }
+    }
+}
--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -1,60 +1,18 @@
-use sycamore::futures::spawn_local;
 use sycamore::prelude::*;
 
-use ebobo_shared::Fighter;
-
 use crate::api::*;
+use crate::components::fighters::Fighters;
 
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
     let index = get().await.unwrap();
-    let select = !index.fighter;
+    //let select = !index.fighter;
 
     view! {
             p {
                 (index.greet)
             }
-
-            Fight(fighters = index.fighters)
-    }
-}
-
-#[component(inline_props)]
-pub async fn Fight<G: Html>(fighters: Vec<Fighter>) -> View<G> {
-    let available = create_signal(fighters);
-
-    view!( div {
-        p { "choose your fighter:" }
-        ul {
-            Indexed(
-                iterable = *available,
-                view = move |f| view! {
-                    SelectFighter(fighter = f.clone())
-                }
-            )
-        }
-    })
-}
-
-#[component(inline_props)]
-pub async fn SelectFighter<G: Html>(fighter: Fighter) -> View<G> {
-    let name = fighter.clone().name();
-    let selected: Signal<Option<Fighter>> = create_signal(None);
-
-    create_effect(move || {
-        if let Some(fighter) = selected.get_clone() {
-            spawn_local(async move {
-                match choose(fighter.name()).await {
-                    Ok(_) => (),
-                    Err(err) => log::error!("error: {:?}", err),
-                }
-            });
-        }
-    });
-
-    view! {
-        li {
-            button(on:click = move |_| selected.set(Some(fighter.clone()))) { (name) }
-        }
+            
+            Fighters(fighters = index.fighters)
     }
 }
