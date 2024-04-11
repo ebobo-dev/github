6f90c468d4854921a41d39efeb30bbf1570b8921
dotnicht <dotnicht@ebobo.dev>
2024-04-11 03:46:52 +0300

    conditional

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -6,13 +6,20 @@ use crate::components::fighters::Fighters;
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
     let index = get().await.unwrap();
-    //let select = !index.fighter;
+    let select = !index.fighter;
 
-    view! {
+    match select {
+        true => view! {
             p {
                 (index.greet)
             }
-            
+
             Fighters(fighters = index.fighters)
+        },
+        false => view! {
+            p {
+                (index.greet)
+            }
+        },
     }
 }
