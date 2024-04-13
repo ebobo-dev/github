d8f64e28f1140b0c255e1d906072d5f882c7a492
dotnicht <dotnicht@ebobo.dev>
2024-04-14 00:45:37 +0300

    fix

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -6,17 +6,16 @@ use crate::components::fighters::Fighters;
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
     let index = get().await.expect("Failed to get page index");
-    let select = !index.fighter;
 
-    match select {
-        true => view! {
+    match index.fighter {
+        false => view! {
             p {
                 (index.greet)
             }
 
             Fighters(fighters = index.fighters)
         },
-        false => view! {
+        true => view! {
             p {
                 (index.greet)
             }
