8d5bf56488b0847d7e4c2b9562bbd0711cfe328b
dotnicht <dotnicht@ebobo.dev>
2024-04-23 01:44:39 +0300

    admin

--- server/src/handlers/admin.rs
+++ server/src/handlers/admin.rs
@@ -1,34 +1,26 @@
-use rocket::{response::status::BadRequest, serde::json::Json, State, serde::Serialize};
+use rocket::{response::status::BadRequest, serde::json::Json, serde::Serialize, State};
 
-use crate::{
-    guards::auth::Auth,
-    AppState,
-};
+use crate::{guards::auth::Auth, AppState};
 
 #[derive(Debug, Serialize)]
 pub struct User<'a> {
     pub id: i32,
     pub name: &'a str,
     pub photo: &'a str,
-    pub fingerprint: String
+    pub fingerprint: String,
 }
 
 #[options("/admin")]
 pub async fn options() {}
 
 #[get("/admin")]
-pub async fn get(
-    auth: Auth,
-    state: &State<AppState>,
-) -> Result<Json<User>, BadRequest<String>> {   
-
-
-    // let user = User {
-    //     id: 0xEB0B0,
-    //     name: "Huxm",
-    //     photo: "🐱",
-    //     fingerprint: auth.fingerprint
-    // };
+pub async fn get(auth: Auth, _state: &State<AppState>) -> Result<Json<User>, BadRequest<String>> {
+    let user = User {
+        id: 0xEB0B0,
+        name: "Huxm",
+        photo: "🐱",
+        fingerprint: auth.fingerprint
+    };
 
     Ok(Json(user))
 }
