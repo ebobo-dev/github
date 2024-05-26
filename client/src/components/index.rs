fb8ca5051b2b2b286f077452f33afc0d21c57cd9
dotnicht <dotnicht@ebobo.dev>
2024-05-26 15:27:28 +0300

    nav2

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -6,12 +6,6 @@ use crate::api::*;
 #[component]
 pub async fn Index<G: Html>() -> View<G> {
     let index = get().await.expect("failed to get greeting");
-
     if !index.fighter { navigate("choose") }
-
-    view! {
-        p {
-            (index.greet)
-        }
-    }
+    view!()
 }
