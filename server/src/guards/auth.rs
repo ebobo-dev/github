349d9a95fec519e08e9819aa4b432e19e561aa0a
dotnicht <dotnicht@ebobo.dev>
2024-05-26 03:56:32 +0300

    format

--- server/src/guards/auth.rs
+++ server/src/guards/auth.rs
@@ -26,31 +26,31 @@ impl<'r> FromRequest<'r> for Auth {
     async fn from_request(req: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
         match req.headers().get_one(ebobo_shared::AUTH_HEADER) {
             Some(device) => match req.rocket().state::<AppState>() {
-                Some(state) => {
-                    match req.client_ip() {
-                        Some(addr) => match Requests::insert(ActiveModel {
-                            id: ActiveValue::set(Uuid::new_v4()),
-                            fingerprint: ActiveValue::set(device.to_string()),
-                            address: ActiveValue::set(Some(addr.to_string())),
-                            timestamp: ActiveValue::set(Utc::now().naive_utc()),
-                        })
-                        .exec(state.db.as_ref())
-                        .await
-                        {
-                            Ok(_) => request::Outcome::Success(Auth {
-                                fingerprint: device.to_string(),
-                            }),
-                            Err(e) => request::Outcome::Error((
-                                Status::InternalServerError,
-                                AuthError::InternalServerError(e.to_string()),
-                            )),
-                        },
-                        None => request::Outcome::Error((Status::Unauthorized, AuthError::MissingAddress)),
+                Some(state) => match req.client_ip() {
+                    Some(addr) => match Requests::insert(ActiveModel {
+                        id: ActiveValue::set(Uuid::new_v4()),
+                        fingerprint: ActiveValue::set(device.to_string()),
+                        address: ActiveValue::set(Some(addr.to_string())),
+                        timestamp: ActiveValue::set(Utc::now().naive_utc()),
+                    })
+                    .exec(state.db.as_ref())
+                    .await
+                    {
+                        Ok(_) => request::Outcome::Success(Auth {
+                            fingerprint: device.to_string(),
+                        }),
+                        Err(e) => request::Outcome::Error((
+                            Status::InternalServerError,
+                            AuthError::InternalServerError(e.to_string()),
+                        )),
+                    },
+                    None => {
+                        request::Outcome::Error((Status::Unauthorized, AuthError::MissingAddress))
                     }
-                }
+                },
                 None => request::Outcome::Error((
                     Status::InternalServerError,
-                    AuthError::InternalServerError("Missing application state".to_string()),
+                    AuthError::InternalServerError("missing application state".to_string()),
                 )),
             },
             None => request::Outcome::Error((Status::Unauthorized, AuthError::MissingFingerprint)),
