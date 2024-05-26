ad4998de81eb1fbab7f517a785430b99857f0c19
dotnicht <dotnicht@ebobo.dev>
2024-05-26 03:44:21 +0300

    navigate

--- client/src/components/index.rs
+++ client/src/components/index.rs
@@ -1,10 +1,13 @@
 use sycamore::prelude::*;
+use sycamore_router::navigate;
 
 use crate::api::*;
 
 #[component(inline_props)]
 pub async fn Index<G: Html>() -> View<G> {
     let index = get().await.expect("failed to get greeting");
+
+    if !index.fighter { navigate("/choose") }
 
     view! {
         p {
