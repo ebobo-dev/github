ff5254efb3fcdb262d3b8a4a3618ced00771ce93
dotnicht <dotnicht@ebobo.dev>
2024-05-26 21:45:36 +0300

    clean

--- client/src/components/arena.rs
+++ client/src/components/arena.rs
@@ -1,10 +1,4 @@
-use sycamore::futures::spawn_local;
 use sycamore::prelude::*;
-
-use ebobo_shared::Fighter;
-use sycamore_router::navigate;
-
-use crate::api::*;
 
 #[component]
 pub async fn Dashboard<G: Html>() -> View<G> {
